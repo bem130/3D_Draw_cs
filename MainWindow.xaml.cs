efc1a984fe3f39efde28363300925ea262bf0e23
haruki1234 <haruki070617@gmail.com>
2022-05-01 20:25:00 +0900

    プロジェクト ファイルを追加します。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Drawing;
+using System.Windows.Shapes;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace _3D_Draw_cs
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Rendering render;
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+        public void show()
+        {
+            Bitmap showimage = render.GetImage();
+            IntPtr hbitmap = showimage.GetHbitmap();
+            ImageView.Source = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(hbitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+            DeleteObject(hbitmap);
+            showimage.Dispose();
+        }
+        public MainWindow()
+        {
+            render = new Rendering();
+            InitializeComponent();
+            show();
+
+        }
+    }
+
+}
---
+++ Rendering.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+
+namespace _3D_Draw_cs
+{
+    internal class Rendering
+    {
+        DImage dImage;
+        public Dictionary<string, int[]> display;
+        public Rendering()
+        {
+            dImage = new DImage();
+            dImage.setpixcel(10, 10, 255, 255, 0, 0);
+            dImage.setpixcel(0, 10, 255, 255, 0, 0);
+            dImage.setpixcel(100, 10, 255, 255, 0, 0);
+
+            display = new Dictionary<string, int[]>()
+            {
+                {"VGA", new int[] { 640,480,4,3 }},
+            };
+        }
+        public Bitmap GetImage()
+        {
+            return dImage.frame;
+        }
+    }
+    class DImage
+    {
+        public Bitmap frame;
+        public DImage()
+        {
+            makenew(100,100);
+        }
+        public Bitmap makenew(int x, int y)
+        {
+            frame = new Bitmap(x+1, y+1);
+            frame = fillRectangle(0, 0, x+1, y+1, 255, 255, 255, 255);
+            return frame;
+        }
+        public void saveimage(string path)
+        {
+            try
+            {
+                frame.Save(path);
+            }
+            catch (Exception e) { Debug.Print(e.ToString()); };
+            return;
+        }
+        public Bitmap setpixcel(int x, int y, int a, int r, int g, int b)
+        {
+            frame.SetPixel(x, y, System.Drawing.Color.FromArgb(a, r, g, b));
+            return frame;
+        }
+        public Bitmap fillRectangle(int x1, int y1, int x2, int y2, int a, int r, int g, int b)
+        {
+            Graphics tmpedit = Graphics.FromImage(frame);
+            tmpedit.FillRectangle(new SolidBrush(System.Drawing.Color.FromArgb(a, r, g, b)), x1, y1, x2, y2);
+            tmpedit.Dispose();
+            return frame;
+        }
+    }
+}
