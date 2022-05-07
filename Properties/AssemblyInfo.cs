226ccfe895e279325e5cd0dbba7d3d55e063b3c5
haruki1234 <haruki070617@gmail.com>
2022-05-07 23:31:20 +0900

    add a icon

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("3D_Draw_cs")]
+[assembly: AssemblyTitle("3D Draw Desktop")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("haruki1234")]
 [assembly: AssemblyProduct("3D_Draw_cs")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
